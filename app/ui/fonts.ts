48255f4df20045f130700efefac770b0d56352d5
furkan aydin <furknn2112@gmail.com>
2024-09-04 18:13:51 +0300

    feat: Add some google fonts
    - Create fonts.ts file to use multiple fonts throughout the application
    - Import inter font from fonts.ts file inside app layout page and apply
    the className of intern to body HTML element to globally use that font
    - Import lusitina font from fonts.ts file inside app page and apply
    where needed.

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+// This file will be used to keep the fonts that will be used throughout application
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ["400", "700"], subsets: ["latin"] });
